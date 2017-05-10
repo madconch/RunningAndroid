8286cf94282f041e21c5ed53c62b986f6c7d1bae
madconch <madconch@126.com>
2017-05-10 15:04:50 +0800

    add mvc and demo module

--- utillibrary/src/main/java/com/madconch/running/utillibrary/savesate/BundleUtil.java
+++ utillibrary/src/main/java/com/madconch/running/utillibrary/savesate/BundleUtil.java
@@ -15,60 +15,76 @@ import java.io.Serializable;
  */
 
 public class BundleUtil {
-    @SuppressWarnings("unchecked")
-    public static <T> T getValueFromBundle(String key, Bundle from) {
-        return (T) from.get(key);
+    public static <T> T getValueFromBundle(String key, Bundle from) throws Exception {
+        try {
+            return (T) from.get(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new Exception(e.getMessage() + " at " + key, e.getCause());
+        }
     }
 
     public static void putObjectToBundle(String key, Object value, Bundle target) throws Exception {
         if (value instanceof Byte) {
             target.putByte(key, (Byte) value);
+            return;
         } else if (value instanceof Character) {
             target.putChar(key, (Character) value);
+            return;
         } else if (value instanceof Short) {
             target.putShort(key, (Short) value);
+            return;
         } else if (value instanceof Float) {
             target.putFloat(key, (Float) value);
+            return;
         } else if (value instanceof CharSequence) {
             target.putCharSequence(key, (CharSequence) value);
+            return;
         } else if (value instanceof Bundle) {
             target.putBundle(key, (Bundle) value);
+            return;
         } else if (value instanceof Parcelable) {
             target.putParcelable(key, (Parcelable) value);
-        } else if (value instanceof Size) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                target.putSize(key, (Size) value);
-            } else {
-                throw new Exception("not support this type:" + key + "=" + value);
-            }
-        } else if (value instanceof SizeF) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                target.putSizeF(key, (SizeF) value);
-            } else {
-                throw new Exception("not support this type:" + key + "=" + value);
-            }
+            return;
         } else if (value instanceof Parcelable[]) {
             target.putParcelableArray(key, (Parcelable[]) value);
+            return;
         } else if (value instanceof byte[]) {
             target.putByteArray(key, (byte[]) value);
+            return;
         } else if (value instanceof short[]) {
             target.putShortArray(key, (short[]) value);
+            return;
         } else if (value instanceof char[]) {
             target.putCharArray(key, (char[]) value);
+            return;
         } else if (value instanceof float[]) {
             target.putFloatArray(key, (float[]) value);
+            return;
         } else if (value instanceof CharSequence[]) {
             target.putCharSequenceArray(key, (CharSequence[]) value);
-        } else if (value instanceof IBinder) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
-                target.putBinder(key, (IBinder) value);
-            } else {
-                throw new Exception("not support this type:" + key + "=" + value);
-            }
+            return;
         } else if (value instanceof Serializable) {
             target.putSerializable(key, (Serializable) value);
-        } else {
-            throw new Exception("not support this type:" + key + "=" + value);
+            return;
         }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (value instanceof Size) {
+                target.putSize(key, (Size) value);
+                return;
+            } else if (value instanceof SizeF) {
+                target.putSizeF(key, (SizeF) value);
+                return;
+            }
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            if (value instanceof IBinder) {
+                target.putBinder(key, (IBinder) value);
+                return;
+            }
+        }
+
+        throw new Exception("not support this type:" + key + "=" + value);
     }
 }
